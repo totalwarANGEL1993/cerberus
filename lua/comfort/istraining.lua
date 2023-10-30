a7f1e9ad96e773b93c37a5f16092ebe629b7522c
totalwarANGEL <n3crosh8d3@gmail.com>
2023-10-30 20:06:13 +0100

    Add comfort IsTraining
    - added function that checks if entity is training

---
+++ lua/comfort/istraining.lua
@@ -0,0 +1,16 @@
+Lib.Register("comfort/IsTraining");
+
+-- Version: 1.0.0
+-- Author:  totalwarANGEL
+
+--- Returns if the entity is training.
+--- @param _Entity any Entity to check
+--- @return boolean Fighting Entity is training
+function IsTraining(_Entity)
+    local ID = GetID(_Entity);
+    if ID ~= 0 then
+        return Logic.LeaderGetBarracks(ID) ~= 0;
+    end
+    return false;
+end
+
