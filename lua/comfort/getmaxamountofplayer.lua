1036dd4a46f23d8ebe40ed3ce5581cce0c6bb0a4
totalwarANGEL <n3crosh8d3@gmail.com>
2023-11-12 13:42:16 +0100

    fix GetMaxAmountOfPlayer

--- lua/comfort/getmaxamountofplayer.lua
+++ lua/comfort/getmaxamountofplayer.lua
@@ -3,6 +3,6 @@ Lib.Register("comfort/GetMaxAmountOfPlayer");
 --- Returns the max amount of possible player IDs.
 --- @return integer Amount Amount of IDs
 function GetMaxAmountOfPlayer()
-    return (CNetwork and XNetwork.Manager_DoesExist() and 16) or 8;
+    return (CNetwork and XNetwork.Manager_DoesExist() == 1 and 16) or 8;
 end
 
