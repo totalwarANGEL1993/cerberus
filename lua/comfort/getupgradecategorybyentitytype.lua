8b29276fc99b315e962c797b068f66cbaa0eefe6
totalwarANGEL <n3crosh8d3@gmail.com>
2024-01-21 11:48:47 +0100

    Fix category mapping

--- lua/comfort/getupgradecategorybyentitytype.lua
+++ lua/comfort/getupgradecategorybyentitytype.lua
@@ -6,6 +6,27 @@ Lib.Register("comfort/GetUpgradeCategoryByEntityType");
 GetUpgradeCategoryByEntityType_IsBuildingCache = {};
 GetUpgradeCategoryByEntityType_IsSettlerCache = {};
 
+GetUpgradeCategoryByEntityType_CategoryMap = {
+    [Entities.CU_BanditLeaderBow1] = UpgradeCategories.LeaderBanditBow,
+    [Entities.CU_BanditSoldierBow1] = UpgradeCategories.SoldierBanditBow,
+    [Entities.CU_BanditLeaderSword1] = UpgradeCategories.LeaderBandit,
+    [Entities.CU_BanditSoldierSword1] = UpgradeCategories.SoldierBandit,
+    [Entities.CU_BanditLeaderSword2] = UpgradeCategories.LeaderBandit,
+    [Entities.CU_BanditSoldierSword2] = UpgradeCategories.SoldierBandit,
+    [Entities.CU_Barbarian_LeaderClub1] = UpgradeCategories.LeaderBarbarian,
+    [Entities.CU_Barbarian_SoldierClub1] = UpgradeCategories.SoldierBarbarian,
+    [Entities.CU_Barbarian_LeaderClub2] = UpgradeCategories.LeaderBarbarian,
+    [Entities.CU_Barbarian_SoldierClub2] = UpgradeCategories.SoldierBarbarian,
+    [Entities.CU_BlackKnight_LeaderMace1] = UpgradeCategories.BlackKnightLeaderMace1,
+    [Entities.CU_BlackKnight_SoldierMace1] = UpgradeCategories.BlackKnightSoldierMace1,
+    [Entities.CU_BlackKnight_LeaderMace2] = UpgradeCategories.BlackKnightLeaderMace1,
+    [Entities.CU_BlackKnight_SoldierMace2] = UpgradeCategories.BlackKnightSoldierMace1,
+    [Entities.PV_Cannon1] = UpgradeCategories.Cannon1,
+    [Entities.PV_Cannon2] = UpgradeCategories.Cannon2,
+    [Entities.PV_Cannon3] = UpgradeCategories.Cannon3,
+    [Entities.PV_Cannon4] = UpgradeCategories.Cannon4,
+};
+
 --- Returns the upgrade category of the entity type
 --- @param _Type number Entity type
 --- @return number Category Upgrade category
@@ -31,6 +52,9 @@ function GetUpgradeCategoryByEntityType(_Type)
         -- Parse entity type for settlers
         -- (Custom settler types must be numbered starting from 1!)
         if GetUpgradeCategoryByEntityType_IsSettlerCache[_Type] then
+            if GetUpgradeCategoryByEntityType_CategoryMap[_Type] then
+                return GetUpgradeCategoryByEntityType_CategoryMap[_Type];
+            end
             local Key = string.sub(TypeName, 4);
             local s,e = string.find(Key, "^[A-Za-z_]+");
             local Suffix = string.sub(Key, e+1);
